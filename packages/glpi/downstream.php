6a654bce8c810f01e438280eb32c1b6933895ca6
Arturo Moya <0x616d@gmail.com>
2026-05-07 22:53:09 -0400

    Add glpi image

---
+++ packages/glpi/downstream.php
@@ -0,0 +1,20 @@
+<?php
+define('GLPI_CONFIG_DIR', '/etc/glpi');
+define('GLPI_LOG_DIR', '/var/log/glpi');
+define('GLPI_VAR_DIR', '/var/lib/glpi');
+define('GLPI_CACHE_DIR', GLPI_VAR_DIR . '/cache');
+define('GLPI_CRON_DIR', GLPI_VAR_DIR . '/cron');
+define('GLPI_DOC_DIR', GLPI_VAR_DIR . '/docs');
+define('GLPI_GRAPH_DIR', GLPI_VAR_DIR . '/graphs');
+define('GLPI_INVENTORY_DIR', GLPI_VAR_DIR . '/inventories');
+define('GLPI_LOCAL_I18N_DIR', GLPI_VAR_DIR . '/locales');
+define('GLPI_LOCK_DIR', GLPI_VAR_DIR . '/lock');
+define('GLPI_MARKETPLACE_DIR', GLPI_VAR_DIR . '/marketplace');
+define('GLPI_PICTURE_DIR', GLPI_VAR_DIR . '/pictures');
+define('GLPI_PLUGIN_DOC_DIR', GLPI_VAR_DIR . '/plugins');
+define('GLPI_RSS_DIR', GLPI_VAR_DIR . '/rss');
+define('GLPI_SESSION_DIR', GLPI_VAR_DIR . '/sessions');
+define('GLPI_THEMES_DIR', GLPI_VAR_DIR . '/themes');
+define('GLPI_TMP_DIR', GLPI_VAR_DIR . '/tmp');
+define('GLPI_UPLOAD_DIR', GLPI_VAR_DIR . '/uploads');
+?>
